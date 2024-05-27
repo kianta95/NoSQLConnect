d3786a02d9684abcc0e2ab5f554dae88cc9d46cf
Kianta McDuffie <mcduffie.kianta@gmail.com>
2024-05-27 12:37:32 -0400

    add folder

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thoughts_routes');
+const userRoutes = require('./user_routes');
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;

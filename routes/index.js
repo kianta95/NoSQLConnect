eaf2644c2accec6f75dad91a9773e73fe83cabcc
Kianta McDuffie <mcduffie.kianta@gmail.com>
2024-05-27 12:36:21 -0400

    updating files

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const thoughtRoutes = require('./thoughts_routes');
-const userRoutes = require('./user_routes');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/thoughts', thoughtRoutes);
-router.use('/users', userRoutes);
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send("<h1>404 Error!</h1>");
+});
 
 module.exports = router;
